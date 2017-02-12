566d875d4c54c106ca62004390906a4d3b10cfae
SvitlanaT <svitlana.tarapata@gmail.com>
2017-02-12 14:15:30 +0200

    first commit

---
+++ Lesson1/src/main/java/pack/Main.java
@@ -0,0 +1,11 @@
+package pack;
+
+/**
+ * @author Света on 12.02.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
